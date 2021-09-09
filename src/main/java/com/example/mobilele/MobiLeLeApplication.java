36aab2e802c4b0720a08abfc1fc0dc12e007aa06
velimir <val.stein10@gmail.com>
2021-09-09 22:12:09 +0300

    initial

---
+++ src/main/java/com/example/mobilele/MobiLeLeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobilele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiLeLeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobiLeLeApplication.class, args);
+    }
+
+}
